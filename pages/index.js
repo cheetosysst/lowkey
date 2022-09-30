c5e2f4acfb0746e588f0a2774bdab12547626f0a
Thect <thect@protonmail.com>
2022-09-30 22:04:07 +0800

    add title to index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Head from "next/head";
+
 import MainLayout from "../components/main.layout.jsx";
 import Test from "../components/test.components.jsx";
 
 export default function Home() {
 	return (
 		<>
+			<Head>
+				<title>Lowkey</title>
+			</Head>
 			<MainLayout>
 				<div className="container xl:w-3/5 md:w-2/3 mx-auto mt-60 text-2xl tracking-widest text-primary-content text-center">
 					<Test />
