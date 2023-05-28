bfea55aa8e1bb7c3349ba9a8477e3511d8a57e6a
cheetosysst <thect@protonmail.com>
2023-05-28 15:44:00 +0800

    feat: leader board udpate duel data rewrite table stuff

--- pages/api/test/leaderboard.ts
+++ pages/api/test/leaderboard.ts
@@ -2,26 +2,48 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Auth, getAuth } from "../../../libs/auth";
 import conn from "../../../libs/database";
 
+export type TestsEntry = { user_id: string; wpm: number; accuracy: number };
+export type DuelEntry = {
+	player1_name: string;
+	player2_name: string;
+	player1_wpm: number;
+	player2_wpm: number;
+	duel_time: string;
+	winner: string;
+};
+
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
 	const auth: Auth | undefined = getAuth(req);
 
-	const data = await conn.transaction(async (tx) => {
+	const [top10, recentDuel] = await conn.transaction(async (tx) => {
 		const top10 = await tx.execute(
 			`SELECT user_id, wpm, accuracy FROM TypeTest ORDER BY wpm desc LIMIT 10;`
 		);
 
-		const userTop = await tx.execute(
-			`SELECT user_id, wpm, accuracy FROM TypeTest WHERE user_id="${auth?.username}" ORDER BY wpm DESC LIMIT 1;`
-		);
+		const recentDuel = await tx.execute(`
+			SELECT
+				Duel.player1 AS player1_name,
+				Duel.player2 AS player2_name,
+				TypeTest1.wpm AS player1_wpm,
+				TypeTest2.wpm AS player2_wpm,
+				Duel.duel_time AS duel_time,
+				Duel.winner AS winner
+			FROM Duel
+			JOIN TypeTest AS TypeTest1 ON Duel.p1_test = TypeTest1.id
+			JOIN TypeTest AS TypeTest2 ON Duel.p2_test = TypeTest2.id
+			WHERE Duel.player2 IS NOT NULL
+			ORDER BY Duel.duel_time DESC
+			LIMIT 10;
+		`);
 
-		return [top10, auth ? userTop : undefined];
+		return [top10.rows, recentDuel.rows];
 	});
 
 	res.status(200).json({
-		top10: data[0]?.rows,
-		userTop: data[1]?.rows[0],
+		top10: top10 as Array<TestsEntry>,
+		recentDuel: recentDuel as Array<DuelEntry>,
 	});
 }
--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,19 +1,21 @@
 import Head from "next/head";
 import MainLayout from "../components/main.layout";
-import { useEffect, useState } from "react";
+import {
+	Component,
+	FC,
+	ReactElement,
+	ReactNode,
+	useEffect,
+	useState,
+} from "react";
 import { getBaseUrl } from "../libs/url";
 import Link from "next/link";
 import { CommonHead } from "../components/meta";
-
-type BoardEntry = {
-	user_id: string;
-	wpm: number;
-	accuracy: string;
-};
+import type { DuelEntry, TestsEntry } from "./api/test/leaderboard";
 
 type BoardData = {
-	top10: Array<BoardEntry>;
-	userTop: BoardEntry | undefined;
+	top10: Array<TestsEntry>;
+	recentDuel: Array<DuelEntry>;
 };
 
 export default function Page() {
@@ -37,8 +39,9 @@ export default function Page() {
 					<article className="text-xl text-gray-400">
 						<section>
 							<h1 className="text-3xl">Leaderboard</h1>
-							<div className="mt-6 flex justify-center">
-								<Board data={leaderboard} />
+							<div className="mt-6 flex">
+								<TableTest data={leaderboard?.top10} />
+								<TableDuel data={leaderboard?.recentDuel} />
 							</div>
 						</section>
 					</article>
@@ -48,61 +51,146 @@ export default function Page() {
 	);
 }
 
-function Board({ data }: { data: BoardData | undefined }) {
-	// TODO Refactor
-	const table =
-		data === undefined
-			? new Array(10).fill({ user_id: "-", wpm: NaN, accuracy: "0" })
-			: data.top10.length < 10
-			? data.top10.concat(
-					new Array(10 - data.top10.length).fill({
-						user_id: "-",
-						wpm: NaN,
-						accuracy: "0",
-					})
-			  )
-			: data.top10;
-
+function Table({ children, title }: { children: ReactNode; title: string }) {
 	return (
-		<>
-			<table className="w-96 table-auto overflow-hidden rounded-md">
-				<tbody>
-					<tr className="bg-slate-800">
-						<th className="font-mono font-normal">rank</th>
-						<th className="font-mono font-normal">name</th>
-						<th className="font-mono font-normal">wpm</th>
-						<th className="font-mono font-normal">acc</th>
-					</tr>
-					{table.map((item, index) => {
-						return (
-							<TableEntry
-								data={item}
-								rank={index + 1}
-								key={`board-${index}`}
-							/>
-						);
-					})}
-				</tbody>
+		<div className="mx-auto mb-10 w-[25em]">
+			<div className="mb-3 text-2xl">{title}</div>
+			<table className="w-[25em] table-auto overflow-hidden rounded-md">
+				{children}
 			</table>
-		</>
+		</div>
 	);
 }
 
-function TableEntry({ data, rank }: { data: BoardEntry; rank: number }) {
+function TableRow({ children, ...props }: { children: ReactNode }) {
 	return (
 		<tr
 			className={`border-b-2 border-white/10 leading-10 transition-all duration-75 hover:bg-slate-700/20`}
+			{...props}
 		>
-			<td className="font-mono font-normal">{rank}</td>
+			{children}
+		</tr>
+	);
+}
+
+function TableTest({ data }: { data: Array<TestsEntry> | undefined }) {
+	if (data === undefined) return <></>;
+
+	const entries = (
+		data.length < 10
+			? data.concat(
+					new Array(10 - data.length).fill({
+						user_id: "",
+						wpm: NaN,
+						accuracy: NaN,
+					})
+			  )
+			: data
+	).map((item, index) => {
+		return (
+			<TableRow key={`testentry-${index}`}>
+				<td className="font-mono font-normal">{index + 1}</td>
+				<TestsEntryElement data={item} />
+			</TableRow>
+		);
+	});
+	return (
+		<Table title="monthly top 10">
+			<thead className="h-8 bg-slate-800">
+				<th className="font-mono font-normal"> </th>
+				<th className="border-l-2 border-cyan-100/10 font-mono font-normal">
+					name
+				</th>
+				<th className="border-l-2 border-cyan-100/10 font-mono font-normal">
+					wpm
+				</th>
+				<th className="border-l-2 border-cyan-100/10 font-mono font-normal">
+					acc
+				</th>
+			</thead>
+			<tbody>{entries}</tbody>
+		</Table>
+	);
+}
+
+function TableDuel({ data }: { data: Array<DuelEntry> | undefined }) {
+	if (data === undefined) return <></>;
+	const entries = (
+		data.length < 10
+			? data.concat(
+					new Array(10 - data.length).fill({
+						player1_id: "",
+						player2_id: "",
+						player1: NaN,
+						player2: NaN,
+						duel_time: "",
+						winner: "",
+					})
+			  )
+			: data
+	).map((item, index, arr) => {
+		console.log(arr);
+		return (
+			<TableRow key={`duelentry-${index}`}>
+				<TableEntryDuel data={item} />
+			</TableRow>
+		);
+	});
+	return (
+		<Table title="recent duels">
+			<thead className="h-8 bg-slate-800">
+				<th className="border-cyan-100/10 font-mono font-normal">id</th>
+				<th className="border-l-2 border-cyan-100/10 font-mono font-normal">
+					wpm
+				</th>
+				<th className="border-l-2 border-cyan-100/10 font-mono font-normal">
+					time
+				</th>
+			</thead>
+			<tbody>{entries}</tbody>
+		</Table>
+	);
+}
+
+function TestsEntryElement({ data }: { data: TestsEntry }) {
+	return (
+		<>
 			<td className="font-mono font-normal">
-				<Link href={`${getBaseUrl()}/user/${data.user_id}`} passHref>
-					{data.user_id}
-				</Link>
+				<UserLink name={data.user_id} />
 			</td>
 			<td className="font-mono font-normal">{data.wpm}</td>
 			<td className="font-mono font-normal">
 				{Number(data.accuracy) * 100}%
 			</td>
-		</tr>
+		</>
+	);
+}
+
+function TableEntryDuel({ data }: { data: DuelEntry }) {
+	return (
+		<>
+			<td className="font-mono font-normal ">
+				<UserLink name={data.player1_name} /> vs{" "}
+				<UserLink name={data.player2_name} />
+			</td>
+			<td className="font-mono font-normal ">
+				{data.player1_wpm}/{data.player2_wpm}
+			</td>
+			<td className="font-mono font-normal">
+				{data.duel_time !== ""
+					? Intl.DateTimeFormat("zh-TW").format(
+							new Date(data.duel_time)
+					  )
+					: "-"}
+			</td>
+		</>
+	);
+}
+
+function UserLink({ name }: { name: string }) {
+	return (
+		<Link href={`${getBaseUrl()}/user/${name}`}>
+			<a>{name}</a>
+		</Link>
 	);
 }
